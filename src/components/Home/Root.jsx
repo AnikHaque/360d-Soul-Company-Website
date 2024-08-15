201754efe7cc9d179d4dcfab99445846198a91cf
AnikHaque <ekramul15-811@diu.edu.bd>
2024-08-15 18:10:33 +0600

    added some components

---
+++ src/components/Home/Root.jsx
@@ -0,0 +1,15 @@
+import About from "./About/About";
+import Explore from "./Explore/Explore";
+import Goal from "./Goal/Goal";
+
+const Root = () => {
+  return (
+    <div>
+      <About />
+      <Goal />
+      <Explore />
+    </div>
+  );
+};
+
+export default Root;
